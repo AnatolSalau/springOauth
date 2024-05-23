b07ccf16c430294568f20207fd0f8648dfd1e300
AnatolSalau <salovanatoly5@mail.ru>
2024-05-23 14:29:36 +0300

    Add client credential flow

---
+++ clientCredentialFlow/src/main/java/com/example/clientcredentialflow/ClientCredentialFlowApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientcredentialflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCredentialFlowApplication {
+
+      public static void main(String[] args) {
+            SpringApplication.run(ClientCredentialFlowApplication.class, args);
+      }
+
+}
---
+++ clientCredentialFlow/src/test/java/com/example/clientcredentialflow/ClientCredentialFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientcredentialflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientCredentialFlowApplicationTests {
+
+      @Test
+      void contextLoads() {
+      }
+
+}
